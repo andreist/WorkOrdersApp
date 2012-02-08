7ab8d4d9eef5ae76392638dc576772db55f44ca7
andreist <andreist@hotmail.com>
2012-02-08 12:51:54 +0200

    initial project version

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SQL
+//
+//  Created by iPhone SDK Articles on 10/26/08.
+//  Copyright 2008 www.iPhoneSDKArticles.com.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
